34457e464688a3d29647e81091042cfeda76b825
JorviGames <jorviaviles@gmail.com>
2023-10-18 22:36:16 -0600

    Inicializacion del proyecto

---
+++ src/main/java/com/Clinicas/demo/ClinicasApplication.java
@@ -0,0 +1,13 @@
+package com.Clinicas.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Clinicas/demo/ClinicasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Clinicas.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
